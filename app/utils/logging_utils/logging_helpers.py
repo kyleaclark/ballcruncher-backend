40277314df2a2aa080d1832819d26b1a2f467987
Kyle Clark <kyleaclark@users.noreply.github.com>
2025-02-16 11:20:49 -0700

    fix py313 dataclass type hint

--- app/utils/logging_utils/logging_helpers.py
+++ app/utils/logging_utils/logging_helpers.py
@@ -1,7 +1,8 @@
 from dataclasses import asdict, dataclass, is_dataclass
+from typing import Type
 
 
-def generate_log_extra(metadata: dict | dataclass) -> dict:
+def generate_log_extra(metadata: dict | Type[dataclass]) -> dict:
     if is_dataclass(metadata):
         metadata = asdict(metadata)
 
